eb36c9028eb8d7efaf32c32af4a633093ef9abad
Luigi Lake <lfdlake@gmail.com>
2017-10-29 18:26:47 -0400

    edits in production.rb to allow assets upload in deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
