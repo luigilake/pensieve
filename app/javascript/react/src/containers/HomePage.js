da8c8eeba0d3c9b6da121aac3f327b6abf05a707
Luigi Lake <lfdlake@gmail.com>
2017-10-30 17:04:45 -0400

    create initial homepage

--- app/javascript/react/src/containers/HomePage.js
+++ app/javascript/react/src/containers/HomePage.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
+import HomeSignedIn from './HomeSignedIn'
+import TimelinesIndex from '../components/TimelinesIndex'
 
 class HomePage extends Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      current_user: {},
+      timelines: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(`/api/v1/users.json`, {
+      credentials: 'same-origin',
+      method: 'GET',
+      headers: { 'Content-Type':'application/json'}
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({ current_user: response })
+    })
+
+    fetch(`/api/v1/timelines`)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({ timelines: response })
+    })
   }
 
   render(){
+    let homeStatus;
+    if(this.state.current_user){
+      homeStatus = <HomeSignedIn timelines={this.state.timelines}/>
+    } else {
+      homeStatus = <TimelinesIndex timelines={this.state.timelines}/>
+    }
     return(
       <div>
         <h1>* The Pensieve Project *</h1>
+        {homeStatus}
       </div>
     )
   }
