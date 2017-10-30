c63cb0985e02d25507dee3eee3b0e5afb71832e2
Luigi Lake <lfdlake@gmail.com>
2017-10-30 17:43:39 -0400

    initial foundation test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w( vendor/modernizr.js )
   # Code is not reloaded between requests.
   config.cache_classes = true
 
